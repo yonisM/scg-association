0dd7dcf933e5cd6e3d531e4a019f24d36f99aa0f
yonisM <yonis@yoniss-MacBook-Pro.local>
2017-05-20 21:25:35 +0100

    new
    new

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -3,19 +3,60 @@ angular.module('app.controllers', [])
 
 .controller('homeCtrl',['$scope', '$stateParams', function ($scope, $stateParams) { 
 
- 
+ //Controls the speed of image changers.
+      $('.carousel').carousel({
+        interval: 5000 //changes the speed
+    });
+    
 }])
 
 
 .controller('donateCtrl',['$scope', '$stateParams', function ($scope, $stateParams) { 
 
+    $scope.single = true;
+    $scope.monthly = false;
+    $scope.annual = false;
+    
+
+    $scope.donationOptionSelection = function(){
+        
+        selectedDonationOption = $scope.selected;
+        
+        switch (selectedDonationOption){
+            case "single":
+                $scope.single = true;
+                $scope.monthly = false;
+                $scope.annual = false;
+                
+            break;
+                
+            case "monthly":
+                $scope.single = false;
+                $scope.monthly = true;
+                $scope.annual = false;
+                
+            break; 
+                
+            case "annual":  
+                $scope.single = false;
+                $scope.monthly = false;
+                $scope.annual = true;
+
+            break;  
+                
+        };
+        
+    };
+   
  
+
 }])
 
 
 
 .controller('aboutCtrl',['$scope', '$stateParams', function ($scope, $stateParams) { 
 
+    
  
 }])
 
